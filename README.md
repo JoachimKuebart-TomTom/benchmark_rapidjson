7639d4b63ac3b27a6a437058e4e819bf96865208
Joachim Kuebart <joachim.kuebart@tomtom.com>
2023-03-09 15:05:40 +0100

    feat: support reading via std::ifsream

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 benchmark_rapidjson
 ===================
 
-This program compares the runtime of various parsing methods using rapidjson.
-The test file is a 735MB JSON map.
+This program compares the runtime of various parsing methods using
+[rapidjson][RJ]. The test file is a 735MB JSON map.
 
 The naïve approach using `rapidjson::IStreamWrapper` for `std::cin` yields:
 
@@ -10,3 +10,15 @@ The naïve approach using `rapidjson::IStreamWrapper` for `std::cin` yields:
 $ ./build/benchmark_rapidjson <Nevada.json
 parsed file: 217482ms
 ```
+
+Most of the time seems to be lost because `std::cin` is unbuffered. Using a
+`std::ifstream` results in a huge speedup:
+
+```
+$ ./build/benchmark_rapidjson Nevada.json
+reading Nevada.json
+parsed file: 14957ms
+```
+
+
+[RJ]: http://rapidjson.org/
--- main.cc
+++ main.cc
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <fstream>
 #include <iostream>
 
 #include <rapidjson/document.h>
@@ -13,11 +14,11 @@ using rapidjson::Writer;
 using std::chrono::duration_cast;
 using std::chrono::milliseconds;
 
-int main() {
+void benchmark(std::istream& is) {
   auto const time0{std::chrono::steady_clock::now()};
   Document doc;
-  IStreamWrapper is{std::cin};
-  doc.ParseStream(is);
+  IStreamWrapper isw{is};
+  doc.ParseStream(isw);
   auto const time1{std::chrono::steady_clock::now()};
   std::cout << "parsed file: " << duration_cast<milliseconds>(time1 - time0).count() << "ms\n";
 
@@ -25,6 +26,18 @@ int main() {
   Writer<OStreamWrapper> writer{os};
   doc.Accept(writer);
   std::cout << '\n';
+}
+
+int main(int argc, char const* argv[]) {
+  if (argc <= 1) {
+    benchmark(std::cin);
+  } else {
+    for (int i{1}; i != argc; ++i) {
+      std::cout << "reading " << argv[i] << '\n';
+      std::ifstream is{argv[i]};
+      benchmark(is);
+    }
+  }
 
   return 0;
 }
